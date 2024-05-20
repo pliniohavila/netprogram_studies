2caacaec972ade4386de219793797a8d88b92346
pliniohavila <pliniohrcode@gmail.com>
2024-05-20 13:08:58 -0300

    finish httprequest

--- ruserver/http/src/httprequest.rs
+++ ruserver/http/src/httprequest.rs
@@ -93,19 +93,50 @@ fn process_req_line(s: &str) -> (Method, Resource, Version) {
     )
 }
 
+fn process_header_line(s: &str) -> (String, String) {
+    let mut header_items = s.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+
+    if let Some(k) = header_items.next() {
+        key = k.to_string();
+    }
+    if let Some(v) = header_items.next() {
+        value = v.to_string();
+    }
+
+    (key, value)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_method_into() {
-        let m: Method = "GET".into();
-        assert_eq!(m, Method::Get);
-    }
+    // #[test]
+    // fn test_method_into() {
+    //     let m: Method = "GET".into();
+    //     assert_eq!(m, Method::Get);
+    // }
 
+
+    // #[test]
+    // fn test_version_into()  {
+    //    let m: Version = "HTTP/1.1".into();
+    //    assert_eq!(m, Version::V1_1); 
+    // }
     #[test]
-    fn test_version_into()  {
-       let m: Version = "HTTP/1.1".into();
-       assert_eq!(m, Version::V1_1); 
+    fn test_read_http() {
+        let s: String = String::from(
+        "GET /greeting HTTP/1.1\r\nHost: localhost:3000\r\nUser-Agent: curl/7.64\r\nAccept: */*\r\n\r\n"
+        );
+        let mut headers_expected = HashMap::new();
+        headers_expected.insert("Host".into(), " localhost".into());
+        headers_expected.insert("Accept".into(), " */*".into());
+        headers_expected.insert("User-Agent".into(), " curl/7.64".into());
+        let req: HttpRequest = s.into();
+        assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
     }
 }
 
